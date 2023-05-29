7ff484b6cc44e1489af85b3263074b8c56b2786c
LinkDev03 <113397561+LinkDev03@users.noreply.github.com>
2023-05-29 14:37:20 -0500

    proyecto full stack

--- Taller Back/calculadora.php
+++ Taller Back/calculadora.php
@@ -2,26 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--CND BOOTSTARP-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/calculadora.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="css/calculadora.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <script src="https://kit.fontawesome.com/6adb63d45f.js" crossorigin="anonymous"></script>
     <title>Calculadora</title>
 </head>
 <body>
-    <a href="index.html#section-respuestas">   
+    <!-- <a href="index.html#section-respuestas">   
         <img class="home" src="img/hogar.png" alt="">
-    </a>
+    </a> -->
+    <a href="index.html"><i id="boton-inicio" class="fa-solid fa-house"></i></a>
     <div class="main-frame-cal">
     <form action="" method="POST">
         <div class="form-group">
             <label for="exampleFormControlInput1">Número 1:</label>
-            <input type="number" name="n1" class="form-control" id="exampleFormControlInput1" placeholder="Digite número 1">
+            <input type="number" name="n1" class="form-control" id="exampleFormControlInput1" placeholder="Digite número 1"/>
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput2">Número 2:</label>
-            <input type="number" name="n2" class="form-control" id="exampleFormControlInput2" placeholder="Digite número 2">
+            <input type="number" name="n2" class="form-control" id="exampleFormControlInput2" placeholder="Digite número 2"/>
         </div>
         <div class="form-group">
             <label for="exampleFormControlSelect1">Seleccione la operación:</label>
--- Taller Back/method_get.php
+++ Taller Back/method_get.php
@@ -7,12 +7,11 @@
     <!--CND BOOTSTARP-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/calculadora.css">
+    <script src="https://kit.fontawesome.com/6adb63d45f.js" crossorigin="anonymous"></script>
     <title>Mayor o menor de edad</title>
 </head>
 <body>
-<a href="index.html#section-respuestas">
-        <img class="home" src="img/hogar.png" alt="">
-    </a>
+<a href="index.html"><i id="boton-inicio" class="fa-solid fa-house"></i></a>
     <div class="main-frame-cal">
     <form action="" method="GET">
         <div class="form-group">
