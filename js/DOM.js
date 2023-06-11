75c1a7e03d0c7ccc71fbc010448e0fc59705af9b
LinkDev03 <113397561+LinkDev03@users.noreply.github.com>
2023-06-11 10:27:27 -0500

    proyecto full stack

---
+++ js/DOM.js
@@ -0,0 +1,33 @@
+const sslider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth;
+const numVisibleSlides = 5;
+var currentSlide = 0;
+
+sslider.style.width = `${slideWidth * numVisibleSlides + (numVisibleSlides - 1) * 6}px`;
+
+function nextSlide() {
+  if (currentSlide < slides.length - numVisibleSlides) {
+    currentSlide++;
+    sslider.style.transform = `translateX(-${slideWidth * currentSlide + 6 * currentSlide}px)`;
+  } else {
+    currentSlide = 0;
+    sslider.style.transform = 'translateX(0)';
+  }
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    sslider.style.transform = `translateX(-${slideWidth * currentSlide + 6 * currentSlide}px)`;
+  } else {
+    currentSlide = slides.length - numVisibleSlides;
+    sslider.style.transform = `translateX(-${slideWidth * currentSlide + 6 * currentSlide}px)`;
+  }
+}
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
